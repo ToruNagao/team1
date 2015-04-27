12f2088d6879abe183a29d434dae84c0aa1e6f4e
ToruNagao <inmylife777@gmail.com>
2015-04-27 09:08:39 -0700

    Toru: Test splash screen with a test picture has added

--- app/src/main/java/com/example/ninjung/testgooglemapver2/SplashActivity.java
+++ app/src/main/java/com/example/ninjung/testgooglemapver2/SplashActivity.java
@@ -9,25 +9,33 @@ import android.os.Handler;
 import android.os.Bundle;
 import android.view.Window;
 import android.content.Intent;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
-public class SplashActivity extends Activity{
+public class SplashActivity extends Activity {
+    //ProgressBar progressBar;
+    //int progress = 100;
+    //Handler handler = new Handler();
+    //TextView textView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.splash);
-
+        int delay = 1000;
         Handler hdl = new Handler();
-        hdl.postDelayed(new splashHandler(), 1000);
+
+        //x ms delay before runs splashHandler
+        hdl.postDelayed(new splashHandler(), delay);
     }
 
     class splashHandler implements Runnable {
         public void run() {
-            Intent intent = new Intent(getApplication(), MainActivity.class);
-            startActivity(intent);
-            SplashActivity.this.finish();
+                Intent intent = new Intent(getApplication(), MainActivity.class);
+                startActivity(intent);
+                SplashActivity.this.finish();
         }
 
     }
-
 }
