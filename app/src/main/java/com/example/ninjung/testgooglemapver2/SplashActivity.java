2d250b3640a3a0710b168cddbeb4c86faee53342
stephentsay <sectsay@gmail.com>
2015-05-11 16:56:18 -0700

    adding logo's and icons
    Stephen

--- app/src/main/java/com/example/ninjung/testgooglemapver2/SplashActivity.java
+++ app/src/main/java/com/example/ninjung/testgooglemapver2/SplashActivity.java
@@ -5,7 +5,11 @@ import android.os.Handler;
 import android.os.Bundle;
 import android.view.Window;
 import android.content.Intent;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.view.MotionEvent;
 import android.widget.ImageView;
+
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 
@@ -17,8 +21,9 @@ public class SplashActivity extends Activity {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.splash);
-        int delay = 1000;
+        int delay = 12000;
         Handler hdl = new Handler();
+
         ConnectionDetector connect = ConnectionDetector.getInstance(getApplicationContext());
 
         // exits immediately if there is no internet connection
@@ -32,6 +37,22 @@ public class SplashActivity extends Activity {
 
         //x ms delay before runs splashHandler
         hdl.postDelayed(new splashHandler(), delay);
+
+    }
+    @Override
+
+    public boolean onTouchEvent(MotionEvent event) {
+
+        if (event.getAction() == MotionEvent.ACTION_DOWN) {
+
+            Intent intent = new Intent(getApplication(), MainActivity.class);
+            startActivity(intent);
+            SplashActivity.this.finish();
+
+
+        }
+
+        return true;
     }
 
     class splashHandler implements Runnable {
